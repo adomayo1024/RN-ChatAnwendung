eb9c087922bbf36fc8782b74d556aaf6b5322e3d
adomayo1024 <git.hub.leon@gmail.com>
2026-01-04 14:12:20 +0100

    changed GoodbyeInputHandler so that it send the packet to all direct neighbours

--- src/main/java/ChatAnwendung/Impl/Handler/InputHandlers/GoodbyeInputHandler.java
+++ src/main/java/ChatAnwendung/Impl/Handler/InputHandlers/GoodbyeInputHandler.java
@@ -28,7 +28,7 @@ public class GoodbyeInputHandler extends AbstractInputHandler {
 
         log.debug("Finished with canceling heartbeats and timeout");
 
-        for (RoutingEntry neighbour : RoutingTableImpl.getInstance().getAllEntries()) {
+        for (RoutingEntry neighbour : RoutingTableImpl.getInstance().getAllDirectNeighbours()) {
 
             if(RoutingTableImpl.getInstance().isUIDavailable(neighbour.getUID())){
                 byte[] payload = new byte[0];
