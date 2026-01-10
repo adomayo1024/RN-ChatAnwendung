c820a610882b0cacbeeb6d02594baf5a18dc01b9
Leon Scharnweber <git.hub.leon@gmail.com>
2026-01-10 14:18:57 +0100

    refactored InputHandler

--- src/main/java/ChatAnwendung/logic/Api/InputHandler.java
+++ src/main/java/ChatAnwendung/logic/Api/InputHandler.java
@@ -1,5 +1,12 @@
 package ChatAnwendung.logic.Api;
 
+/**
+ * Verarbeitet alle Inputs des Users.
+ */
 public interface InputHandler {
+    /**
+     * Der Loop, wo alle Inputs des Users verarbeitet werden.
+     * @param stdIn Der Input des Users
+     */
     void handle(String stdIn);
 }
